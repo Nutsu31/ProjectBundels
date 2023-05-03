292baa932357a47f344a182ca58b3afeb6ff9cd9
Nutsu31 <116025262+Nutsu31@users.noreply.github.com>
2023-05-03 16:23:13 +0400

    root level navigate to todo path

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "../components/header/NavBar";
+import { useEffect } from "react";
 const Root = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/todo-app");
+  }, []);
   return (
     <>
       <NavBar />
